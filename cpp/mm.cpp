44a08b3e9504aacc9c534480258255f912da2154
winsphinx <winsphinx@gmail.com>
2015-03-10 11:10:28 +0800

    mod cpp

--- cpp/mm.cpp
+++ cpp/mm.cpp
@@ -9,11 +9,10 @@ bool mulint(int x, int y);
 bool divint(int x, int y);
 bool calculate(int x, int y, bool (*pf)(int x, int y));
 int randint(int min, int max){return min + rand() % max;};
-void counting(int ok, int total);
 
 int main()
 {
-    int m, n, o, x, y, ok;
+    int m, n, o, x, y, ok = 0;
     bool (*op[])(int, int) = {addint, subint, mulint, divint};
     srand((int) time(0));
     std::cout << "输入最大位数：";
@@ -27,9 +26,18 @@ int main()
         y = randint(1, pow(10, m) - 1);
         o = rand() % 4;
         if (calculate(x, y, op[o]))
+        {
+            std::cout << "正确！" << std::endl;
             ok++;
+        }
+        else
+        {
+            std::cout << "错误！" << std::endl;
+            /* todo: error expression */
+        }
     }
-    counting(ok, n);
+    std::cout << std::endl << "答对" << ok << "题。" << std::endl;
+    std::cout << "正确率" << 100 * ok / n << "%。"<< std::endl;
 
     return 0;
 }
@@ -39,17 +47,7 @@ bool addint(int x, int y)
     std::cout << x << " + " << y << " = ";
     int z;
     std::cin >> z;
-    if (z == x + y)
-    {
-        std::cout << "正确！" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "错误！" << std::endl;
-        /* TODO: error expression */
-        return false;
-    }
+    return x + y == z;
 }
 
 bool subint(int x, int y)
@@ -64,17 +62,7 @@ bool subint(int x, int y)
     std::cout << x << " - " << y << " = ";
     int z;
     std::cin >> z;
-    if (z == x - y)
-    {
-        std::cout << "正确！" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "错误！" << std::endl;
-        /* TODO: error expression */
-        return false;
-    }
+    return x - y == z;
 }
 
 bool mulint(int x, int y)
@@ -82,17 +70,7 @@ bool mulint(int x, int y)
     std::cout << x << " * " << y << " = ";
     int z;
     std::cin >> z;
-    if (z == x * y)
-    {
-        std::cout << "正确！" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "错误！" << std::endl;
-        /* TODO: error expression */
-        return false;
-    }
+    return x * y == z;
 }
 
 bool divint(int x, int y)
@@ -100,18 +78,7 @@ bool divint(int x, int y)
     std::cout << x << " / " << y << " = ";
     int z;
     std::cin >> z;
-    if (z == x / y)
-    {
-        std::cout << "正确！" << std::endl;
-        return true;
-    }
-    else
-    {
-        std::cout << "错误！" << std::endl;
-        /* TODO: error expression */
-        /* TODO: deal with redudence */
-        return false;
-    }
+    return x / y == z;
 }
 
 bool calculate(int x, int y, bool (*pf)(int x, int y))
@@ -119,8 +86,3 @@ bool calculate(int x, int y, bool (*pf)(int x, int y))
     return (*pf)(x, y);
 }
 
-void counting(int ok, int total)
-{
-    std::cout << std::endl << "答对" << ok << "题" << std::endl;
-    std::cout << "正确率" << (int) (ok / total * 100) << "%"<< std::endl;
-}
