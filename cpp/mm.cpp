7fdabf4d32ce1bc6759de36a2b61688feeee7835
winsphinx <winsphinx@gmail.com>
2015-03-11 22:22:57 +0800

    mod cpp

--- cpp/mm.cpp
+++ cpp/mm.cpp
@@ -4,7 +4,12 @@
 #include "cmath"
 #include "string"
 
-bool isint(std::string s);
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+bool isint(string s);
 bool addint(int x, int y);
 bool subint(int x, int y);
 bool mulint(int x, int y);
@@ -25,57 +30,58 @@ int main(int argc, char *argv[])
             {
                 m = atoi(argv[1]);
                 n = atoi(argv[2]);
-                std::cout << "最大位数为" << m << "位" << std::endl;
-                std::cout << "答题次数为" << n << "次" << std::endl;
+                cout << "最大位数为" << m << "位" << endl;
+                cout << "答题次数为" << n << "次" << endl;
             }
             else
             {
-                std::cout << "Err" << std::endl;
+                cout << "Err" << endl;
                 return 1;
             }
             break;
         case 1:
-            std::cout << "输入最大位数：";
-            std::cin >> m;
-            std::cout << "输入答题次数：";
-            std::cin >> n;
+            cout << "输入最大位数：";
+            cin >> m;
+            cout << "输入答题次数：";
+            cin >> n;
             break;
         default:
-            std::cout << "参数错误！请输入：" << std::endl;
-            std::cout << "\t命令 最大位数 答题次数<回车>" << std::endl;
-            std::cout << "或直接输入：" << std::endl;
-            std::cout << "\t命令<回车>" << std::endl;
+            cout << "格式：" << endl;
+            cout << "\tmm 最大位数 答题次数" << endl;
+            cout << "或直接输入：" << endl;
+            cout << "\tmm" << endl;
+            cout << "再根据提示输入参数" << endl;
             return 1;
     }
 
     for (int i = 1; i <= n; ++i)
     {
-        std::cout << "第" << i << "题：  ";
+        cout << "第" << i << "题：  ";
         x = randint(1, pow(10, m) - 1);
         y = randint(1, pow(10, m) - 1);
         o = rand() % 4;
         if (calculate(x, y, op[o]))
         {
-            std::cout << "正确！" << std::endl;
+            cout << "正确！" << endl;
             ok++;
         }
         else
         {
-            std::cout << "错误！" << std::endl;
+            cout << "错误！" << endl;
             /* todo: error expression */
         }
     }
-    std::cout << std::endl << "答对" << ok << "题。" << std::endl;
-    std::cout << "正确率" << 100 * ok / n << "%。"<< std::endl;
+    cout << endl << "答对" << ok << "题。" << endl;
+    cout << "正确率" << 100 * ok / n << "%。"<< endl;
 
     return 0;
 }
 
 bool addint(int x, int y)
 {
-    std::cout << x << " + " << y << " = ";
+    cout << x << " + " << y << " = ";
     int z;
-    std::cin >> z;
+    cin >> z;
     return x + y == z;
 }
 
@@ -88,25 +94,25 @@ bool subint(int x, int y)
         x = y;
         y = t;
     }
-    std::cout << x << " - " << y << " = ";
+    cout << x << " - " << y << " = ";
     int z;
-    std::cin >> z;
+    cin >> z;
     return x - y == z;
 }
 
 bool mulint(int x, int y)
 {
-    std::cout << x << " * " << y << " = ";
+    cout << x << " * " << y << " = ";
     int z;
-    std::cin >> z;
+    cin >> z;
     return x * y == z;
 }
 
 bool divint(int x, int y)
 {
-    std::cout << x << " / " << y << " = ";
+    cout << x << " / " << y << " = ";
     int z;
-    std::cin >> z;
+    cin >> z;
     return x / y == z;
 }
 
@@ -115,7 +121,7 @@ bool calculate(int x, int y, bool (*pf)(int x, int y))
     return (*pf)(x, y);
 }
 
-bool isint(std::string s)
+bool isint(string s)
 {
     for(int i=0; i<s.length(); i++)
     {
