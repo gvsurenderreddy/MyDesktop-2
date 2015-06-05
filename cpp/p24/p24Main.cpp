64b1b6a6b21ed5fd6d3e9f5d75e833672c737f09
winsphinx <winsphinx@gmail.com>
2015-06-05 15:28:18 +0800

    mod cpp

--- cpp/p24/p24Main.cpp
+++ cpp/p24/p24Main.cpp
@@ -137,55 +137,53 @@ p24Frame::p24Frame(wxWindow* parent, wxWindowID id)
     //(*Initialize(p24Frame)
     wxGridSizer* GridSizer1;
     wxBoxSizer* BoxSizer1;
-
     Create(parent, id, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE, _T("id"));
     BoxSizer1 = new wxBoxSizer(wxVERTICAL);
-    TextCtrl1 = new wxTextCtrl(this, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxSize(280,40), wxTE_READONLY|wxTE_RIGHT, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    wxFont TextCtrl1Font(20,wxFONTFAMILY_SWISS,wxFONTSTYLE_NORMAL,wxFONTWEIGHT_NORMAL,false,_T("Monaco"),wxFONTENCODING_DEFAULT);
+    TextCtrl1 = new wxTextCtrl(this, ID_TEXTCTRL1, wxEmptyString, wxDefaultPosition, wxSize(280, 40), wxTE_READONLY | wxTE_RIGHT, wxDefaultValidator, _T("ID_TEXTCTRL1"));
+    wxFont TextCtrl1Font(20, wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL, false, _T("Monaco"), wxFONTENCODING_DEFAULT);
     TextCtrl1->SetFont(TextCtrl1Font);
-    BoxSizer1->Add(TextCtrl1, 1, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+    BoxSizer1->Add(TextCtrl1, 1, wxALL | wxALIGN_CENTER_HORIZONTAL, 5);
     GridSizer1 = new wxGridSizer(4, 3, 0, 0);
     Button1 = new wxButton(this, ID_BUTTON1, _("1"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON1"));
-    GridSizer1->Add(Button1, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button1, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button2 = new wxButton(this, ID_BUTTON2, _("2"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
-    GridSizer1->Add(Button2, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button2, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button3 = new wxButton(this, ID_BUTTON3, _("3"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON3"));
-    GridSizer1->Add(Button3, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button3, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button4 = new wxButton(this, ID_BUTTON4, _("4"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON4"));
-    GridSizer1->Add(Button4, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button4, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button5 = new wxButton(this, ID_BUTTON5, _("5"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON5"));
-    GridSizer1->Add(Button5, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button5, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button6 = new wxButton(this, ID_BUTTON6, _("6"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON6"));
-    GridSizer1->Add(Button6, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button6, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button7 = new wxButton(this, ID_BUTTON7, _("7"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON7"));
-    GridSizer1->Add(Button7, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button7, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button8 = new wxButton(this, ID_BUTTON8, _("8"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON8"));
-    GridSizer1->Add(Button8, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button8, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     Button9 = new wxButton(this, ID_BUTTON9, _("9"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON9"));
-    GridSizer1->Add(Button9, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(Button9, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     ButtonNew = new wxButton(this, ID_BUTTON_NEW, _("New"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON_NEW"));
-    GridSizer1->Add(ButtonNew, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(ButtonNew, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     ButtonHelp = new wxButton(this, ID_BUTTON_HELP, _("Help"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON_HELP"));
-    GridSizer1->Add(ButtonHelp, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
+    GridSizer1->Add(ButtonHelp, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
     ButtonQuit = new wxButton(this, ID_BUTTON_QUIT, _("Quit"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON_QUIT"));
-    GridSizer1->Add(ButtonQuit, 1, wxALL|wxALIGN_CENTER_VERTICAL, 5);
-    BoxSizer1->Add(GridSizer1, 3, wxALL|wxALIGN_CENTER_HORIZONTAL, 5);
+    GridSizer1->Add(ButtonQuit, 1, wxALL | wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer1->Add(GridSizer1, 3, wxALL | wxALIGN_CENTER_HORIZONTAL, 5);
     SetSizer(BoxSizer1);
     BoxSizer1->Fit(this);
     BoxSizer1->SetSizeHints(this);
-
-    Connect(ID_BUTTON1,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton1Click);
-    Connect(ID_BUTTON2,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton2Click);
-    Connect(ID_BUTTON3,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton3Click);
-    Connect(ID_BUTTON4,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton4Click);
-    Connect(ID_BUTTON5,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton5Click);
-    Connect(ID_BUTTON6,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton6Click);
-    Connect(ID_BUTTON7,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton7Click);
-    Connect(ID_BUTTON8,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton8Click);
-    Connect(ID_BUTTON9,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnButton9Click);
-    Connect(ID_BUTTON_NEW,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnNew);
-    Connect(ID_BUTTON_HELP,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnAbout);
-    Connect(ID_BUTTON_QUIT,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&p24Frame::OnQuit);
+    Connect(ID_BUTTON1, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton1Click);
+    Connect(ID_BUTTON2, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton2Click);
+    Connect(ID_BUTTON3, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton3Click);
+    Connect(ID_BUTTON4, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton4Click);
+    Connect(ID_BUTTON5, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton5Click);
+    Connect(ID_BUTTON6, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton6Click);
+    Connect(ID_BUTTON7, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton7Click);
+    Connect(ID_BUTTON8, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton8Click);
+    Connect(ID_BUTTON9, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnButton9Click);
+    Connect(ID_BUTTON_NEW, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnNew);
+    Connect(ID_BUTTON_HELP, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnAbout);
+    Connect(ID_BUTTON_QUIT, wxEVT_COMMAND_BUTTON_CLICKED, (wxObjectEventFunction)&p24Frame::OnQuit);
     //*)
 }
 
@@ -242,7 +240,12 @@ void p24Frame::OnButton1Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -259,7 +262,12 @@ void p24Frame::OnButton2Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -276,7 +284,12 @@ void p24Frame::OnButton3Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -293,7 +306,12 @@ void p24Frame::OnButton4Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -310,7 +328,12 @@ void p24Frame::OnButton5Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -327,7 +350,12 @@ void p24Frame::OnButton6Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -344,7 +372,12 @@ void p24Frame::OnButton7Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -361,7 +394,12 @@ void p24Frame::OnButton8Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
@@ -378,7 +416,12 @@ void p24Frame::OnButton9Click(wxCommandEvent& event)
         if (++digit == COUNT)
         {
             newflag = 0;
+            ButtonNew->Enable();
             ButtonNew->SetLabel(wxString("Calc"));
+        }
+        else
+        {
+            ButtonNew->Disable();
         }
     }
 }
