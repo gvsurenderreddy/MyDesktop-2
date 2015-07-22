c4e50f6d8effec9c7b0ebddfb5d276d93baa2b76
winsphinx <winsphinx@gmail.com>
2015-07-22 21:23:58 +0800

    mod rb

--- ruby/fact.rb
+++ ruby/fact.rb
@@ -1,12 +1,11 @@
 def fact(n)
-   if n == 0
-      1
-   elsif n > 0
-      n * fact(n-1)
-   else
-      print "Error!"
-   end
+    if n == 0
+        1
+    elsif n > 0
+        n * fact(n-1)
+    else
+        print "Error!"
+    end
 end
 
 print fact(ARGV[0].to_i), "\n"
-
