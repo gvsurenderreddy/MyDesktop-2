a18085b10362e0a0557cc31fe13c4f14c34431d1
winsphinx <winsphinx@gmail.com>
2015-07-29 19:13:23 +0800

    mod py

--- python/epi.py
+++ python/epi.py
@@ -3,18 +3,22 @@
 
 from random import random
 from math import sqrt
+from sys import stdout
 
 x = y = inn = out = 0.0
 
-
-for i in range(300000):
+for i in range(30000):
     x = random()
     y = random()
-    print(x, y)
+    # print(x, y)
+    stdout.write(str(i) + ": (" +
+                 str(x) + ", " +
+                 str(y) + ")" +
+                 "      \r")
     if (sqrt(x * x + y * y) < 1.0):
         inn += 1.0
     else:
         out += 1.0
 
-# print(inn, out)
+print()
 print(inn / out)
